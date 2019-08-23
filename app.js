e332af034871721e19a14f027e1961925cccfd26
HishD <35591927+hishd@users.noreply.github.com>
2019-08-23 11:09:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
-const http = require('http');
+var http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 80;
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Hello World\n');
+}).listen(8080);
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Welcome to HishD Node Test\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+console.log('Server started');
